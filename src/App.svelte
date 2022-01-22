7b74d5432dbb0bb30e6b3c43a6bfb645c6859386
Matthew Vaden <mvaden@mindgrub.com>
2022-01-22 15:29:28 -0500

    Set up handler for getting hits from Count API.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script>
 	
+	async function getHits() {
+		let response = await fetch('https://api.countapi.xyz/hit/1ccb732e-b55a-4404-ad3f-0f99c02fe44e');
+		let hits = await response.json();
+	}
 </script>
 
 <main>
-	<h1>Hello world!</h1>
+	<div>
+		<button on:click={getHits}>Update hits</button>
+	</div>
 </main>
 
 <style>
