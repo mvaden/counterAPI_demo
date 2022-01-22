8c9a7f2b51fd09f97a3db95a705484d2e719c5d8
Matthew Vaden <mvaden@mindgrub.com>
2022-01-22 15:34:02 -0500

    Render a loading message for the user while getting the hit count value.

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,28 @@
 <script>
+	let data = {
+		loading: false
+	} 
 	
 	async function getHits() {
+		data.loading = true;
 		let response = await fetch('https://api.countapi.xyz/hit/1ccb732e-b55a-4404-ad3f-0f99c02fe44e');
 		let hits = await response.json();
+		data = {
+			loading: false
+		}
 	}
 </script>
 
 <main>
 	<div>
 		<button on:click={getHits}>Update hits</button>
+		<div>
+			<span>Hits: </span>
+			
+			{#if data.loading === true}
+			<span>Getting hits...</span>
+			{/if}
+		<div>
 	</div>
 </main>
 
