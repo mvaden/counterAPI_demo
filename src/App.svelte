10de22b167add2b6a4cf72f32d1edc724a5a0e54
Matthew Vaden <mvaden@mindgrub.com>
2022-01-22 15:36:10 -0500

    When the hit count response has returned a status 200, print the value to the DOM.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,15 @@
 <script>
 	let data = {
+		incrementCount: undefined,
 		loading: false
 	} 
-	
+
 	async function getHits() {
 		data.loading = true;
 		let response = await fetch('https://api.countapi.xyz/hit/1ccb732e-b55a-4404-ad3f-0f99c02fe44e');
 		let hits = await response.json();
 		data = {
+			incrementCount: hits.value,
 			loading: false
 		}
 	}
@@ -21,6 +23,10 @@
 			
 			{#if data.loading === true}
 			<span>Getting hits...</span>
+
+			{:else if data.incrementCount !== undefined}
+			<span>{data.incrementCount}</span>
+
 			{/if}
 		<div>
 	</div>
